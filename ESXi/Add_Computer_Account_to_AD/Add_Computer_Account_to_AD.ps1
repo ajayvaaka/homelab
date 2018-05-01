2efb1aa9512569e112bc3aa5b7cfba7e58c05e8f
ajayvaaka <ajayvaaka@gmail.com>
2018-04-30 22:20:44 -0400

    Update Add_Computer_Account_to_AD.ps1

--- ESXi/Add_Computer_Account_to_AD/Add_Computer_Account_to_AD.ps1
+++ ESXi/Add_Computer_Account_to_AD/Add_Computer_Account_to_AD.ps1
@@ -2,5 +2,16 @@
 # Description : Adding Computer accounts to Active Directory in lab environment ,considering Active Directory is virtual within vCenter server
 # Dependencies: Powercli 10.0.0 and above 
 ################################################################################
-
+###Collect Info from json file
 $source = Get-Content -raw ".\content.json"  -ErrorAction Stop |ConvertFrom-Json
+
+###Connect to vCenter Server appliance
+connect-viserver $source.QA.vc -User $source.QA.user -Password $source.QA.password 
+
+$computer = foreach($x in $source.QA.computeraccount)
+
+{
+
+Invoke-VMScript -VM $source.QA.ADVM -GuestUser $source.QA.ADuser -GuestPassword $source.QA.ADpassword -ScriptText 'New-ADcomputer –name $computer -path $source.QA.path'
+
+}
